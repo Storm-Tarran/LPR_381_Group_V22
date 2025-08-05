d8d32889e2a779b62f4f08a408da2f94d5ea44df
Storm Tarran <stormtarran03@gmail.com>
2025-08-05 19:38:04 +0200

    Add project files.

---
+++ LPR_381_Group_V22/IO/InputFileParser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LPR_381_Group_V22.IO
+{
+    internal class InputFileParser
+    {
+    }
+}
---
+++ LPR_381_Group_V22/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LPR_381_Group_V22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
